abaeaff5a2ea3d010f7f4f95cc112f894a6d4279
Vignesh M <mv20502886@gmail.com>
2026-02-21 12:07:50 +0530

    update

--- index.js
+++ index.js
@@ -11,10 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://stack-quest-frontend.vercel.app"
-  ],
+  origin:true,
   credentials: true
 }));
 
